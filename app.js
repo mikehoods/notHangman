6d7edcc932c9c9176c891ab5c51e7bcffebb4954
Michael Hood <michaelhood@Michaels-MacBook-Pro.local>
2020-05-02 00:34:26 -0500

    added input container, box and button for entering word. set click event to show input container. more css styling

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-console.log("test1")
-$(()=> {
 
-    console.log("test2")
+$(()=> {
+    // Establish jQuery variables for #playNow button and #enterWord-container
+    const $playNow = $('#playNow')
+    const $enterWordContainer = $('#enterWord-container')
+    // Click event to display #enterWord-container and hide playNow button
+    $playNow.on('click', ()=> {
+        $enterWordContainer.css('display', 'block')
+        $playNow.css('display', 'none')
+    })
+    //set input to only accept letters
+    // function alphaOnly(event) {
+    //     var key = event.keyCode;
+    //     return ((key >= 65 && key <= 90) || key == 8);
+    //   };
+
+
+
+
+
 })
